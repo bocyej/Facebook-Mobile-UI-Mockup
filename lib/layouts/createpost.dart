b218e98c8d1022df9cac113e2e23064cbdd4f6b2
Jacob Laurel <jacobkristopherlaurel@gmail.com>
2022-09-11 00:50:37 +0800

    created 'what\'s on your mind'

---
+++ lib/layouts/createpost.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class CreatePost extends StatelessWidget {
+  const CreatePost({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(left: 10, top: 10, right: 5, bottom: 7),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Material(
+              shape: const CircleBorder(),
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                onTap: () {},
+                child: Ink.image(
+                  image: const AssetImage('assets/icons/icon1.jpg'),
+                  height: 40,
+                  width: 40,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 5, top: 10, right: 5, bottom: 7),
+            child: InkWell(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+                // );
+              },
+              child: Container(
+                padding: const EdgeInsets.only(left: 15, top: 9, bottom: 10),
+                width: 220,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                  border: Border.all(color: Colors.grey),
+                  color: Colors.white,
+                ),
+                child: const Text(
+                  'What\'s on your mind?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 10, top: 10, right: 5, bottom: 7),
+            child: InkWell(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const RewardsPage()),
+                // );
+              },
+              child: Container(
+                child: const Icon(
+                  Icons.filter_rounded,
+                  color: Colors.green,
+                  size: 25,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
