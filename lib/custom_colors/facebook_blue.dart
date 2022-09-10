c4f0d8213cd44ee9d5cef45e645e7a1248f3c3b8
Jacob Laurel <jacobkristopherlaurel@gmail.com>
2022-09-10 21:33:23 +0800

    new custom color

---
+++ lib/custom_colors/facebook_blue.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryBlack = MaterialColor(
+  _facebookPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF1877F5),
+    100: Color(0xFF1877F5),
+    200: Color(0xFF1877F5),
+    300: Color(0xFF1877F5),
+    400: Color(0xFF1877F5),
+    500: Color(0xFF1877F5),
+    600: Color(0xFF1877F5),
+    700: Color(0xFF1877F5),
+    800: Color(0xFF1877F5),
+    900: Color(0xFF1877F5),
+  },
+);
+const int _facebookPrimaryValue = 0xFF1877F5;
