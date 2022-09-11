d0609ca6819aead9102743969267d5c680b83666
Jacob Laurel <jacobkristopherlaurel@gmail.com>
2022-09-11 16:54:13 +0800

    updated structure

--- lib/pages/structure.dart
+++ lib/pages/structure.dart
@@ -1,12 +1,80 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
+import '../custom_colors/facebook_blue.dart';
+
+import '../layouts/appbar.dart';
+
+import '../pages/homepage.dart';
+import '../pages/marketplace.dart';
+import '../pages/profilepage.dart';
+import '../pages/feedpage.dart';
+import '../pages/notificationpage.dart';
+import '../pages/menupage.dart';
+
 class Structure extends StatelessWidget {
   const Structure({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(home: StructurePage());
+  }
+}
+
+class StructurePage extends StatefulWidget {
+  const StructurePage({Key? key}) : super(key: key);
+
+  @override
+  State<StructurePage> createState() => _StructurePageState();
+}
+
+class _StructurePageState extends State<StructurePage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: ,
+      appBar: const FAppBar(),
+      body: DefaultTabController(
+        length: 6, // length of tabs
+        initialIndex: 0,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: TabBar(
+                labelColor: facebookBlue,
+                unselectedLabelColor: Colors.grey.shade700,
+                tabs: const [
+                  Tab(icon: Icon(Icons.home)),
+                  Tab(icon: Icon(Icons.storefront_outlined)),
+                  Tab(icon: Icon(Icons.account_circle_outlined)),
+                  Tab(icon: Icon(Icons.newspaper)),
+                  Tab(icon: Icon(Icons.notifications_outlined)),
+                  Tab(icon: Icon(Icons.person)),
+                ],
+              ),
+            ),
+            Container(
+              height: 564,
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(color: Colors.grey, width: 0.5),
+                ),
+              ),
+              child: const TabBarView(
+                children: <Widget>[
+                  HomePage(),
+                  MarketplacePage(),
+                  HomePage(),
+                  FeedsPage(),
+                  NotificationsPage(),
+                  HomePage(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
