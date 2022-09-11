eb5b6f5a47dec2e0fb112fd6a824791b5b176cfa
Jacob Laurel <jacobkristopherlaurel@gmail.com>
2022-09-11 16:47:28 +0800

    created new custom color

---
+++ lib/custom_colors/unread_blue.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor facebookLightBlue = MaterialColor(
+  _facebookSecondaryValue,
+  <int, Color>{
+    50: Color(0xFFE7F3FF),
+    100: Color(0xFFE7F3FF),
+    200: Color(0xFFE7F3FF),
+    300: Color(0xFFE7F3FF),
+    400: Color(0xFFE7F3FF),
+    500: Color(0xFFE7F3FF),
+    600: Color(0xFFE7F3FF),
+    700: Color(0xFFE7F3FF),
+    800: Color(0xFFE7F3FF),
+    900: Color(0xFFE7F3FF),
+  },
+);
+const int _facebookSecondaryValue = 0xFFE7F3FF;
